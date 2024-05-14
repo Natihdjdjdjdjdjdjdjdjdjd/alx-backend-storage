c47adb9e4ea3bf8e35de400b1fa2b444d482906a
Natihdjdjdjdjdjdjdjdjdjd <natasbest@gmail.com>
2024-05-14 19:02:54 +0000

    10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-'''Task 10's module manggo opration'''
+'''ajkjjlad Task 10's module.'''
 
 
 def update_topics(mongo_collection, name, topics):
-     """ Changes all topics of a school document based on the name """
-   my_ query = {"name": name}
-    new_values = {"$set": {"topics": topics}}
-
-    mongo_collection.update_many(my_query, new_values)
+    '''Changes all topics of a collection's document based on the name.
+    '''
+    mongo_collection.update_many(
+        {'name': name},
+        {'$set': {'topics': topics}}
+    )
