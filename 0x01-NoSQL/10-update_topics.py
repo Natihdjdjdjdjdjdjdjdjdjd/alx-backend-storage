af70136a201447c37c171a785a465bb24ca9fcc8
Natihdjdjdjdjdjdjdjdjdjd <natasbest@gmail.com>
2024-05-14 19:00:38 +0000

    asad

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -3,9 +3,8 @@
 
 
 def update_topics(mongo_collection, name, topics):
-    '''Changes all topics of a collection's document based on the name.
-    '''
-      my_query = {'name': name},
-      add = {'$set': {'topics': topics}}
+     """ Changes all topics of a school document based on the name """
+   my_ query = {"name": name}
+    new_values = {"$set": {"topics": topics}}
 
-       mongo_collection.update_many(my_query, add)
+    mongo_collection.update_many(my_query, new_values)
