aa185b7d92adc71552faeeb3b599e349d091c498
Natihdjdjdjdjdjdjdjdjdjd <natasbest@gmail.com>
2024-05-17 19:35:58 +0000

    dddd

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Caching request module
+Our Caching request module
 """
 import redis
 import requests
@@ -8,25 +8,25 @@ from functools import wraps
 from typing import Callable
 
 
-def track_get_page(fn: Callable) -> Callable:
+def our_page(fn: Callable) -> Callable:
     """ Decorator for get_page
     """
     @wraps(fn)
     def my_apper(url: str) -> str:
         """ my_apper url's data is cache
         """
-        client = redis.Redis()
-        client.incr(f'count:{url}')
-        cached_page = client.get(f'{url}')
+        myclient = redis.Redis()
+        myclient.incr(f'count:{url}')
+        cached_page = myclient.get(f'{url}')
         if cached_page:
             return cached_page.decode('utf-8')
         response = fn(url)
-        client.set(f'{url}', response, 10)
+        myclient.set(f'{url}', response, 10)
         return response
     return my_apper
 
 
-@track_get_page
+@our_page
 def get_page(url: str) -> str:
     """ request to a given endpoint
     """
