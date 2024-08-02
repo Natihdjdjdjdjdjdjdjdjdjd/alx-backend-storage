03f7ccbd37ad9cf39fe25402af00218cf28ce85f
Natihdjdjdjdjdjdjdjdjdjd <natasbest@gmail.com>
2024-08-02 20:20:15 +0000

    letpusj

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,33 +1,27 @@
 #!/usr/bin/env python3
-"""
-provides some stats about Nginx logs stored in MongoDB
-"""
+"""Log stats module"""
 from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    """
-    Ensure only executable if not imported
-    """
-    tab = " " * 4
     client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
-    all_logs = list(nginx_collection.find())
-    log_count = nginx_collection.estimated_document_count()
-    print("{} logs".format(log_count))
+    db = client.logs.nginx
+
+    num_logs = db.count_documents({})
+    print(f"{num_logs} logs")
+
+    get = db.count_documents({'method': 'GET'})
+    post = db.count_documents({'method': 'POST'})
+    put = db.count_documents({'method': 'PUT'})
+    patch = db.count_documents({'method': 'PATCH'})
+    delete = db.count_documents({'method': 'DELETE'})
+
     print("Methods:")
-    get_docs = nginx_collection.count_documents({"method": "GET"})
-    post_docs = nginx_collection.count_documents({"method": "POST"})
-    put_docs = nginx_collection.count_documents({"method": "PUT"})
-    patch_docs = nginx_collection.count_documents({"method": "PATCH"})
-    delete_docs = nginx_collection.count_documents({"method": "DELETE"})
-    get_status_docs = nginx_collection.count_documents({"method": "GET",
-                                                        "path": "/status"})
-    print("\tmethod GET: {}".format(get_docs).expandtabs(4))
-    print("\tmethod POST: {}".format(post_docs).expandtabs(4))
-    print("\tmethod PUT: {}".format(put_docs).expandtabs(4))
-    print("\tmethod PATCH: {}".format(patch_docs).expandtabs(4))
-    print("\tmethod DELETE: {}".format(delete_docs).expandtabs(4))
-    print("{} status check".format(get_status_docs))
-    # for log in all_logs:
-    #     print(f"{log}")
+    print(f"\tmethod GET: {get}")
+    print(f"\tmethod POST: {post}")
+    print(f"\tmethod PUT: {put}")
+    print(f"\tmethod PATCH: {patch}")
+    print(f"\tmethod DELETE: {delete}")
+
+    status = db.count_documents({'method': 'GET', 'path': '/status'})
+    print(f"{status} status check")
